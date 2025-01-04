2a04b1b130d9ab14ef105409b5bbe48560b55b40
Jose Diaz <joseddg92@gmail.com>
2025-01-04 20:46:25 +0100

    more information on jet werror
    Signed-off-by: Jose Diaz <joseddg92@gmail.com>

--- crates/rpc/rpc-layer/src/jwt_validator.rs
+++ crates/rpc/rpc-layer/src/jwt_validator.rs
@@ -1,7 +1,7 @@
 use crate::{AuthValidator, JwtError, JwtSecret};
 use http::{header, HeaderMap, Response, StatusCode};
 use jsonrpsee_http_client::{HttpBody, HttpResponse};
-use tracing::error;
+use tracing::info;
 
 /// Implements JWT validation logics and integrates
 /// to an Http [`AuthLayer`][crate::AuthLayer]
@@ -21,41 +21,16 @@ impl JwtAuthValidator {
 }
 
 impl AuthValidator for JwtAuthValidator {
-    fn validate(&self, headers: &HeaderMap) -> Result<(), HttpResponse> {
-        match get_bearer(headers) {
-            Some(jwt) => match self.secret.validate(&jwt) {
-                Ok(_) => Ok(()),
-                Err(e) => {
-                    error!(target: "engine::jwt-validator", "Invalid JWT: {e}");
-                    let response = err_response(e);
-                    Err(response)
-                }
-            },
-            None => {
-                let e = JwtError::MissingOrInvalidAuthorizationHeader;
-                error!(target: "engine::jwt-validator", "Invalid JWT: {e}");
-                let response = err_response(e);
-                Err(response)
-            }
-        }
+    fn validate(&self, _headers: &HeaderMap) -> Result<(), HttpResponse> {
+        info!(
+            target: "engine::jwt-validator",
+            "Bypassing JWT validation. All requests are accepted."
+        );
+        Ok(())
     }
 }
 
-/// This is an utility function that retrieves a bearer
-/// token from an authorization Http header.
-fn get_bearer(headers: &HeaderMap) -> Option<String> {
-    let header = headers.get(header::AUTHORIZATION)?;
-    let auth: &str = header.to_str().ok()?;
-    let prefix = "Bearer ";
-    let index = auth.find(prefix)?;
-    let token: &str = &auth[index + prefix.len()..];
-    Some(token.into())
-}
-
 fn err_response(err: JwtError) -> HttpResponse {
-    // We build a response from an error message.
-    // We don't cope with headers or other structured fields.
-    // Then we are safe to "expect" on the result.
     Response::builder()
         .status(StatusCode::UNAUTHORIZED)
         .body(HttpBody::new(err.to_string()))
